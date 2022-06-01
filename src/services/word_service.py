12e81f6d9bcd3746f004e6edc5690389ff38235f
Shreya Sharma <shreya.sharma@krantitechservices.com>
2022-06-01 17:12:35 +0530

    unit and component testing

--- src/services/word_service.py
+++ src/services/word_service.py
@@ -1,7 +1,7 @@
 """
 This module represents a sample Servcie class
 """
-
+from typing import Tuple
 from src.models.sample_model import new_word_response
 
 word_list = []
@@ -15,7 +15,7 @@ class word_service():
     def __init__(self) -> None:
         pass
 
-    def detail(word: str) -> tuple[new_word_response, bool]:
+    def detail(word: str) -> Tuple[new_word_response, bool]:
         """Get detail for the given word
 
         Args:
